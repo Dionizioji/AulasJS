f8a9df74ea23f6698c4a923ce6ebc49169e590a2
Dionízio <dionizioji@gmail.com>
2022-04-02 22:44:52 -0300

    Atualização aula 87

--- aula87/index.js
+++ aula87/index.js
@@ -15,3 +15,12 @@ const tudodeUmaVez = numeros.filter(function(valor){
     return acumulador + valor
 },0)
 console.log(tudodeUmaVez)
+
+//Usando MAP, FILTER e REDUCE (na forma de arrow functions)
+//O uso de arrow NESSE CASO encurta a sintaxe e faz a mesma coisa, portanto, melhor! 
+const numeros2 = [5,50,80,1,2,3,5,8,7,11,15,22,27]
+const usandoOsTres = numeros
+.filter(valor => valor%2===0)
+.map(valor => valor*2)
+.reduce((acumulador, valor) => acumulador + valor)
+console.log(usandoOsTres)
